56a4b092680b7f890097a6f8dc12795a1ead08cf
keke <dondohare55@gmail.com>
2018-09-20 08:25:26 +0900

    some translation

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: schedules
@@ -9,27 +11,27 @@
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 
-
 class Schedule < ApplicationRecord
   enum action_type: {
-      go_out: 0,
-      meeting: 1,
-      vacation: 2,
-      early_leaving: 3,
+    go_out: 0,
+    meeting: 1,
+    vacation: 2,
+    early_leaving: 3
   }
   validates :action_type, inclusion: action_types.keys
   validate :time_check
 
   def time_check
-    if self.termination_time <= DateTime.now
-      errors.add(:termination_time, "は現在より過去の日時を指定できません")
+    if termination_time <= DateTime.now
+      errors.add(:termination_time, 'は現在より過去の日時を指定できません')
     end
   end
 
-
-  def format_time
-    self.termination_time.strftime('%Y/%m/%d %H:%M')
+  def user_group_name
+    current_user.group.name
   end
 
-
+  def format_time
+    termination_time.strftime('%Y/%m/%d %H:%M')
+  end
 end
