43d780b7912905977e8c9db4c4fe0a9dfb545761
koji koike <k.koike@daitecjp.com>
2018-10-02 15:02:22 +0900

    bugfix: modify wrong name and id

--- app/javascript/TerminationTimeSetting/HourMin.jsx
+++ app/javascript/TerminationTimeSetting/HourMin.jsx
@@ -92,13 +92,13 @@ class HourMin extends React.Component {
   render() {
     return (
       <span>
-          <select id="schedule_start_time_4i" name="schedule[start_time(4i)]"
+          <select id="schedule_termination_time_4i" name="schedule[termination_time(4i)]"
                   className="browser-default inline-date"
                   defaultValue={this.props.defaultHour}>
           {getOptionsWithPaddingZero(Array.from({length: 24}, (v, k) => k), 'hours')}
         </select>
         &ensp;:&ensp;
-        <select id="schedule_start_time_5i" name="schedule[start_time(5i)]"
+        <select id="schedule_termination_time_5i" name="schedule[termination_time(5i)]"
                 className="browser-default inline-date" defaultValue={this.props.defaultMin}>
           {getOptionsWithPaddingZero(Array.from({length: 12}, (v, k) => k * 5), 'minutes')}
         </select>
