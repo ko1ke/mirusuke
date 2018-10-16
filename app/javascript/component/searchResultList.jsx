0c5f83ed211f194064ff073d4e212ecf9af57c8b
koji koike <k.koike@daitecjp.com>
2018-10-16 10:29:13 +0900

    bugfix: blinking scrollbar
    that cause unnecessary rendering, so I change it null rendering

--- app/javascript/component/searchResultList.jsx
+++ app/javascript/component/searchResultList.jsx
@@ -3,18 +3,7 @@ import SearchResult from './searchResult';
 
 export default ({results, loading}) => (
   <div className="results">
-    {loading ? (
-      <div className="spinner-layer spinner-blue">
-        <div className="circle-clipper left">
-          <div className="circle"></div>
-        </div>
-        <div className="gap-patch">
-          <div className="circle"></div>
-        </div>
-        <div className="circle-clipper right">
-          <div className="circle"></div>
-        </div>
-      </div>) : (
+    {loading ? null : (
       <div className="divide">
         {results.map(result => <SearchResult key={result.id} result={result}/>)}
       </div>
